00760a277244d3fc3de72c6334d578265950784f
Allen Zhang (张涛) <tzhangm@trip.com>
2021-12-16 11:39:58 +0800

    添加reporter字段

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
 module.exports = function (api) {
     api.cache(true);
     const presets = [];
-    const plugins = [['./lib', {projectId: 99999, dsn: 'http://xxx.xxx/api/v1/coverage/client',commitSha:'xxxxx'}]];
+    const plugins = [
+        ['./lib',
+            {
+                projectId: 99999,
+                dsn: 'http://xxx.xxx/api/v1/coverage/client',
+                commitSha:'xxxxx',
+                reporter:"reporter"
+            }]];
     return {
         presets,
         plugins
