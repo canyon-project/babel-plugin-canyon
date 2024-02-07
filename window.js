be27141265853aca82e763240417927425113da0
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-02-07 12:57:34 +0800

    feat: update window

--- window.js
+++ window.js
@@ -1,9 +1,13 @@
 (function () {
-    if (typeof window !== 'undefined') {
-        return window;
-    } else if (typeof global !== 'undefined') {
-        return global;
+  try {
+    return (new Function('return this')());
+  } catch (e) {
+    if (typeof window !== "undefined") {
+      return window;
+    } else if (typeof global !== "undefined") {
+      return global;
     } else {
-        throw new Error('Unable to determine global object');
+      throw new Error('unable to locate global object');
     }
+  }
 })()
