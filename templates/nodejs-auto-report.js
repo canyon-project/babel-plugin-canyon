404ddf295e785263ebf7f5ffd113b815af96ac1d
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-07-31 14:12:51 +0800

    feat: udpate

--- templates/nodejs-auto-report.js
+++ templates/nodejs-auto-report.js
@@ -1,22 +1,26 @@
 if (typeof process !== 'undefined' && process.env) {
   function autoReport () {
-    if (fetch&&window.__canyon__&&window.__coverage__) {
-      console.log('auto report ing...')
-      fetch(window.__canyon__.dsn, {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + window.__canyon__.reporter
-        },
-        body: JSON.stringify({
-          coverage: window.__coverage__,
-          ...window.__canyon__
+    try {
+      if (fetch&&window.__canyon__&&window.__coverage__) {
+        console.log('canyon: auto report ing...')
+        fetch(window.__canyon__.dsn, {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + window.__canyon__.reporter
+          },
+          body: JSON.stringify({
+            coverage: window.__coverage__,
+            ...window.__canyon__
+          })
+        }).then(function () {
+          console.log('canyon: auto report success')
+        }).catch(function (err) {
+          console.log('canyon: auto report error:', err)
         })
-      }).then(function () {
-        console.log('auto report success')
-      }).catch(function (err) {
-        console.log('auto report error:', err)
-      })
+      }
+    } catch (e) {
+
     }
   }
   autoReport()
