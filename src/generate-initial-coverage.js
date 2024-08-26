83a42d9a130ebf03ad40182569d89cfd61b03c60
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-08-26 17:06:03 +0800

    upd

--- src/generate-initial-coverage.js
+++ src/generate-initial-coverage.js
@@ -1,24 +1,8 @@
 import generate from '@babel/generator';
 import fs from 'fs';
 import path from 'path'
-import libCoverage from 'istanbul-lib-coverage'
+import {extractCoverageData} from "./helpers/extractCoverageData";
 
-
-function extractCoverageData(scriptContent) {
-    const regex = /var\s+coverageData\s*=\s*({[\s\S]*?});/;
-    const match = regex.exec(scriptContent);
-    if (match) {
-        const objectString = match[1];
-        try {
-            return new Function('return ' + objectString)();
-        } catch (e) {
-            console.error('Failed to parse object:', e);
-            return null;
-        }
-    } else {
-        return null;
-    }
-}
 
 export const generateInitialCoverage = (paramsPath) => {
     const initialCoverageDataForTheCurrentFile = extractCoverageData(generate(paramsPath.node).code)
@@ -28,16 +12,6 @@ export const generateInitialCoverage = (paramsPath) => {
         fs.mkdirSync(dir, {recursive: true});
     }
     if (initialCoverageDataForTheCurrentFile) {
-        const fadata = {
-            [initialCoverageDataForTheCurrentFile.path]: initialCoverageDataForTheCurrentFile
-        }
-        if (fs.existsSync(filePath)) {
-            const oldData = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-            const map = libCoverage.createCoverageMap(oldData);
-            map.merge(libCoverage.createCoverageMap(fadata));
-            fs.writeFileSync(filePath, JSON.stringify(map.toJSON(), null, 2), 'utf-8')
-        } else {
-            fs.writeFileSync(filePath, JSON.stringify(fadata, null, 2), 'utf-8')
-        }
+        fs.writeFileSync(`./.canyon_output/coverage-${Math.random()}.json`, JSON.stringify(initialCoverageDataForTheCurrentFile, null, 2), 'utf-8');
     }
 }
