26e6e3681d5878cf52466612f58984ee6b1c8ff9
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-08-29 14:37:37 +0800

    feat: add save local

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   const presets = [];
   const plugins = [
+    'istanbul',
     ['./lib',
       {
         dsn: 'http://xxx.xxx/api/v1/coverage/client',
