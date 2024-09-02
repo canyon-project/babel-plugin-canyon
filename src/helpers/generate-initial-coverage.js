ac7d7f8cfb212ae326cf18a9afd98fdc32c05fc6
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-02 15:59:48 +0800

    upd

--- src/helpers/generate-initial-coverage.js
+++ src/helpers/generate-initial-coverage.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path'
-import {extractCoverageData} from "./extractCoverageData";
+import {extractCoverageData} from "./extract-coverage-data";
 
 
 export const generateInitialCoverage = (paramsPath) => {
